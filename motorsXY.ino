c0b1df980384d3939b199d334df00571a967ffb4
ukicar <uros@nodesign.net>
2013-03-08 16:34:37 +0100

    english comments

--- motorsXY.ino
+++ motorsXY.ino
@@ -1,100 +1,97 @@
+// find excellent rotary library under https://github.com/brianlow/Rotary
 #include <rotary.h>
 
+// define input pins for LD293D
 #define INPUT_A 8 
 #define INPUT_B 9
 #define INPUT_C 10
 #define INPUT_D 11
 
+// create two rotary objects
 Rotary r = Rotary(2, 3);
 Rotary r2 = Rotary(4, 5);
 
+// this is current position for motors
 volatile int posX, posY;
 
-int X=A0;
-int Y=A1;
-int valeurX = 0;
-int valeurY = 0;
-
-char mem[5];
-
+int targetX, targetY;
 
 void setup (){
   Serial.begin(9600);
   Serial.println("start...");
 
-  // déclarer les pins comme sorties numériques
+  // declare inputs for LD293D
   pinMode(INPUT_A, OUTPUT);
   pinMode(INPUT_B, OUTPUT);
   pinMode(INPUT_C, OUTPUT);
   pinMode(INPUT_D, OUTPUT);
 
-
+  // settings for rotary encoders
   PCICR |= (1 << PCIE2) | (1 << PCIE1);
   PCMSK2 |= (1 << PCINT18) | (1 << PCINT19) | (1 << PCINT20) | (1 << PCINT21);
   sei();
 
 }
 
-// loop est vide car tous ce passe dans les callbacks, serialEvent - quand qqch arrive dans le port série et ISR quand un encodeur 
-// enregistre changement
 
 void loop (){
 
 
 }
 
-/// serial callback
+#define MAX_RCV_BYTES 5 // define length of messages, for example x1234 is 5 bytes long 
+
+/// serial handler
 void serialEvent(){
-// lire le port série jusqu'à le caractere 'e' et stocker les valeurs dans la liste mem[]
-// découper en 5 bytes maximum, si plus que 5 bytes est envoyé il reécrit dans la liste
-// donc envoter le caractere 'e' est cruciale
+  // Serial handler is activates when there is something in serial port
+  // idea is to send coordinates in format :
+  // x100e
+  // y2452e
+  // s
+  // letter 'e' is for end, that's delimiter, s - is for stop
+  // 
+  // read serial port until letter 'e' than put infos in mem array
+  char mem[MAX_RCV_BYTES];
 
-  Serial.readBytesUntil('e', mem, 5);
-// ici FIFO est vidé
+  Serial.readBytesUntil('e', mem, MAX_RCV_BYTES);
+  // after this FIFO will be empty
 
-// voir ce qu'il a reçu
-//  for (int i=0; i<5; i++) 
-//    Serial.println(mem[i]);
+  // print contents of mem
+  //  for (int i=0; i<MAX_RCV_BYTES; i++) 
+  //    Serial.println(mem[i]);
 
-// conversion des caracteres en integers
-// le premier caractere est une lettre, donc il faut créer une nouvelle liste sans
-// le premiere caractere
-  char nmbr[4]; // nouvelle liste qui va contenir que des chiffres
-  for (int i=0; i<4; i++) 
+  // Convert from characters to integers
+  // first caracter is letter so create a new list without him
+  char nmbr[MAX_RCV_BYTES-1]; // new array only for numbers
+  for (int i=0; i<MAX_RCV_BYTES-1; i++) 
     nmbr[i] = mem[i+1];
-  
-  int number = atoi(nmbr); // fonction qui traduit les caracteres en integers
-  
-  Serial.print("received ");
-  Serial.println(number);
+
+  int number = atoi(nmbr); // convert caracters to numbers
+
+  Serial.print("received "); // print what is received
+  Serial.println(number); 
+
+  switch(mem[0]) {
+
+  case 'x' :
+    targetX = number;
+    break;
+
+  case 'y' :
+    targetY = number;
+    break;
+
+  case 's' : // stop
+    
+    break;
+
+  case 'r' : // reset to initial position
+
+    break;
 
 
-  for (int i=0; i<5; i++) 
-    mem[i]=0;
+  }
 
-
-
-  /*
-  int val = Serial.read(); 
-   
-   switch(val) {
-   
-   case 'x' :
-   avanceMotor('x');
-   break;
-   
-   case 'y' :
-   avanceMotor('y');
-   break;
-   
-   case 's' :
-   
-   stopMotor('x');
-   stopMotor('y');
-   break;
-   
-   }
-   */
 }
 
 ISR(PCINT2_vect) {
@@ -126,20 +123,7 @@ ISR(PCINT2_vect) {
   }
 }
 
-void RAZ() {
-
-  /// au debut va doucement dans la direction de l'interrupteur
-  /*
- /// allume le moteur
-   while(interrupteurX!=1) {}
-   posX = 0;
-   
-   while(interrupteurY!=1) {}
-   posY = 0;
-   */
-}
-
-void avanceMotor(int motorXY) { // enter 'X' or 'Y'
+void motorForward(int motorXY) { // enter 'X' or 'Y'
   if ((motorXY=='X') || (motorXY=='x')) {
     digitalWrite(INPUT_A, HIGH); 
     digitalWrite(INPUT_B, LOW);  
@@ -151,7 +135,7 @@ void avanceMotor(int motorXY) { // enter 'X' or 'Y'
 
 }
 
-void reculeMotor(int motorXY) {
+void motorBackward(int motorXY) {
   if ((motorXY=='X') || (motorXY=='x')) {
     digitalWrite(INPUT_A, LOW); 
     digitalWrite(INPUT_B, HIGH);  
@@ -162,7 +146,7 @@ void reculeMotor(int motorXY) {
   }
 }
 
-void stopMotor(int motorXY) {
+void motorStop(int motorXY) {
   if ((motorXY=='X') || (motorXY=='x')) {
     digitalWrite(INPUT_A, LOW); 
     digitalWrite(INPUT_B, LOW);  
@@ -172,10 +156,6 @@ void stopMotor(int motorXY) {
     digitalWrite(INPUT_D, LOW);
   }
 }
-
-
-
-
 
 
 
